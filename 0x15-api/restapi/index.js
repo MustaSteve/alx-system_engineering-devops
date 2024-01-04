6ab7cfe565e7567866ff32d7f8882acd9dbf93b4
MustaSteve <avineusq@gmail.com>
2024-01-04 09:11:52 -0800

    Tasks all

---
+++ 0x15-api/restapi/index.js
@@ -0,0 +1,27 @@
+//site for this https://www.restapitutorial.com/
+// simple Express.js RESTful API
+'use strict';
+
+// initialize
+const
+  port = 8888,
+  express = require('express'),
+  app = express();
+
+  // enable CORS
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', '*');
+    next();
+  });
+  
+// /hello/ GET request
+app.get('/hello/:name?', (req, res) =>
+  res.append('Access-Control-Allow-Origin', '*').json(
+    { message: `Hello ${req.params.name || 'world'}!` }
+  )
+);
+
+// start server
+app.listen(port, () =>
+  console.log(`Server started on port ${port}`)
+);
